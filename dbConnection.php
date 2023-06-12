996d52feda1adc7ff7d56fc5c10d4bfb314837a0
Shahirul Islam Polak <131145933+sip23bjit@users.noreply.github.com>
2023-06-12 16:31:22 +0600

    Update dbConnection.php
    aws

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$databaseHost = 'localhost';
+$databaseHost = 'polak-db.crkfp60kehfn.ap-northeast-2.rds.amazonaws.com';
 $databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = '';
+$databaseUsername = 'example';
+$databasePassword = 'password';
 
 // Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
